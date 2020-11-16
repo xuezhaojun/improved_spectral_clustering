f55a7dbbd0660be31bb8a2bd70114390dc3b6bcf
xuezhaojun <1340762527@qq.com>
2020-11-16 22:52:59 +0800

    add code

--- Readme.md
+++ Readme.md
@@ -1,2 +1,6 @@
-datasets.py: provide functions to get datasets
-scde.py: provide functions related to scde+ 
+datasets.py: 
+    provide functions to get datasets
+scde.py: 
+    provide functions related to scde+ 
+estimate_k:
+    privide functions to estimate k in dataset
--- estimate_k.py
+++ estimate_k.py
@@ -7,22 +7,21 @@ tf.config.experimental_run_functions_eagerly(True)
 # unpper_bound : k_u in paper
 def estimate_mnist(upper_bound,train):
     input = Input(shape=(784,))
-    # encoded = Dense(50, activation='relu')(input)
-    # encoded = Dense(upper_bound, activation='softmax')(encoded)
-    # decoded = Dense(50, activation='relu')(encoded)
-    # decoded = Dense(784, activation='linear')(decoded)
-
-    encoded = Dense(upper_bound,activation="softmax")(input)
-    decoded = Dense(784, activation="linear")(encoded)
+    encoded = Dense(50, activation='relu')(input)
+    encoded = Dense(upper_bound, activation='softmax')(encoded)
+    decoded = Dense(50, activation='relu')(encoded)
+    decoded = Dense(784, activation='linear')(decoded)
 
     autoencoder = Model(input,decoded)
     encoder = Model(input,encoded)
 
     autoencoder.compile(optimizer='sgd',loss=scde.loss_func_in_scde_plus(encoded))
-    autoencoder.fit(train,train,epochs=50, batch_size=256, shuffle=True,validation_data=(train,train))
+    # epochs = 50 一般,为了计算快速，目前设置为5
+    autoencoder.fit(train,train,epochs=5, batch_size=256, shuffle=True,validation_data=(train,train))
 
-    result = encoder.pridect(train)
+    return encoder.predict(train)
 
+def get_k_from_result(result):
     # count for k
     max_indexs = {}
     for ps in result:
@@ -35,5 +34,4 @@ def estimate_mnist(upper_bound,train):
                 max_index = index
             index += 1
         max_indexs[max_index] = True
-
     return len(max_indexs)
